75ae1cf9d2b26da6f4934793fd3338cf6db0c8ef
Jeet Maheshwari <jeetmaheshwari1919@gmail.com>
2026-02-15 14:28:22 +0530

    Initial project setup

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
